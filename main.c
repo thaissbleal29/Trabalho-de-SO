30ad92b7457b50fa40493a27949b47b1d6d1daee
thaissbleal29 <thaisbleal2006@gmail.com>
2026-07-07 20:24:56 -0300

    Add comments to main function for clarity
    Added comments to clarify code functionality.

--- main.c
+++ main.c
@@ -4,24 +4,24 @@
 #include "builtins.h"
 
 int main(void) {
-        char line[MAX_LINE];
-        char *args[MAX_ARGS];
+        char line[MAX_LINE]; // Linha digitada pelo usuário
+        char *args[MAX_ARGS]; // Vetor que armazena os argumentos
 
-        while(1) {
-                printf("MeuJarshell:>");
-                if(fgets(line, sizeof(line), stdin) == NULL) {
+        while(1) { // Loop principal do shell
+                printf("MeuJarshell:>"); 
+                if(fgets(line, sizeof(line), stdin) == NULL) { //Lê a linha digitada 
                         break;
                 }
 
-                parse_command(line, args);
-                if(args[0] == NULL) {
+                parse_command(line, args); // Faz o parser da linha
+                if(args[0] == NULL) { // Ignora as linhas vazias
                         continue;
                 }
 
-                if(execute_builtin(args)) {
+                if(execute_builtin(args)) { // Verifica se o comando é um builtin
                         continue;
                 }
-                exec_command(args);
+                exec_command(args); // Executa o comando quando ele é externo
         }
         return 0;
 }
