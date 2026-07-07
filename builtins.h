2442e69a8c5bfe506ae9bd3fd7ff421be15aa391
thaissbleal29 <thaisbleal2006@gmail.com>
2026-07-07 18:51:53 -0300

    Add comments to execute_builtin function in builtins.h
    Added comments to clarify the purpose and return values of the execute_builtin function.

--- builtins.h
+++ builtins.h
@@ -1,6 +1,10 @@
 #ifndef BUILTINS_H
 #define BUILTINS_H
 
+// Executa um comando interno (builtin).
+// Retorna:
+// 1 -> o comando era um builtin e foi executado.
+// 0 -> o comando não é um builtin.
 int execute_builtin(char *args[]);
 
 #endif
