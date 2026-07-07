61d3bf18086eadd2504196a3fc7a822a0d1e5ae5
thaissbleal29 <thaisbleal2006@gmail.com>
2026-07-07 20:10:11 -0300

    Add comments for clarity in test_parser.c
    Added comments to clarify the purpose of variables and functions.

--- test_parser.c
+++ test_parser.c
@@ -2,16 +2,16 @@
 #include "parser.h"
 
 int main(void)  {
-        char line[MAX_LINE];
-        char *args[MAX_ARGS];
+        char line[MAX_LINE]; // Linha digitada pelo usuário
+        char *args[MAX_ARGS]; // Vetor que armazenará os argumentos
 
         printf("Meu Shell lindo>");
-        fgets(line, sizeof(line), stdin);
+        fgets(line, sizeof(line), stdin); // Lê a linha digitada
 
-        int qtt = parse_command(line, args);
+        int qtt = parse_command(line, args); // Armazena o número de argumentos
         printf("Número de argumentos: %d\n ", qtt);
 
-        for(int i = 0; args[i] != NULL; i++) {
+        for(int i = 0; args[i] != NULL; i++) { // Exibe os argumentos encontrados
                 printf("args[%d] = %s\n", i, args[i]);
         }
         return 0;
