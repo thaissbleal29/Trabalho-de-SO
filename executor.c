0023d4b23639f3ba30f95b5516ffc37f0a085b2c
thaissbleal29 <thaisbleal2006@gmail.com>
2026-07-07 20:01:06 -0300

    Refactor exec_command for better error handling
    Refactor exec_command function to improve error handling and code clarity.

--- executor.c
+++ executor.c
@@ -5,28 +5,42 @@
 #include <sys/wait.h>
 #include "executor.h"
 
+// EXECUTA COMANDOS EXTERNOS UTILIZANDO FORK, EXECVP E WAITPID
 void exec_command(char *args[]) {
-    //caso nao tenha comando
+
+    // Verifica se existe algum comando para executar
     if (args[0] == NULL) {
         return;
     }
 
+    // Cria um novo processo (filho)
     pid_t pid = fork();
 
+    // Verifica se ocorreu erro na criação do processo
     if (pid < 0) {
         perror("Erro no fork");
         return;
-    } 
+    }
+
+    // PROCESSO FILHO
     else if (pid == 0) {
-       int retorno = execvp(args[0], args);
+
+        // Substitui o processo filho pelo programa solicitado
+        int retorno = execvp(args[0], args);
+
+        // Caso o execvp falhe, exibe a mensagem de erro e encerra o processo filho
         if (retorno == -1) {
             perror("Erro ao executar comando");
             exit(EXIT_FAILURE);
         }
-    } 
+    }
+
+    // PROCESSO PAI
     else {
+
         int status;
-        //espera o filho terminar
+
+        // Aguarda o término do processo filho
         if (waitpid(pid, &status, 0) == -1) {
             perror("Erro ao esperar pelo processo filho");
         }
