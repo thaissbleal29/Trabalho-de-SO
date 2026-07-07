105e2d919ea4dbad07cae7aa1f8d42a975176231
thaissbleal29 <thaisbleal2006@gmail.com>
2026-07-07 20:06:19 -0300

    Fix parser.h header guard and add newline

--- parser.h
+++ parser.h
@@ -1,8 +1,12 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+// Tamanho máximo da linha
 #define MAX_LINE 1024
+// Número máximo de argumentos
 #define MAX_ARGS 100
 
+// Realiza o parser da linha digitada
 int parse_command(char *line, char *args[]);
+
 #endif
